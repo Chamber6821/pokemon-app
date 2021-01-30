2ce0aa5c77320aa05928bdae64cd63ec84c88e3f
Chamber6821 <vania.osinin@yandex.ru>
2021-01-30 16:32:01 +0300

    Move images to assets/img

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Layout from "./components/Layout";
-import Pikachu from "./assets/bg1.jpg";
-import Bg3 from "./assets/bg3.jpg";
+import Pikachu from "./assets/img/bg1.jpg";
+import Bg3 from "./assets/img/bg3.jpg";
 
 function App() {
     return (
