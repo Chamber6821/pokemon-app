f42adabea064e1c3fea9d7647046c41c3956c4b0
Chamber6821 <vania.osinin@yandex.ru>
2021-02-10 02:27:08 +0300

    feature(PokemonContext): add PokemonContext to GamePage

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,4 +1,7 @@
 import {Route, Switch, useRouteMatch} from 'react-router-dom';
+import {useState} from 'react';
+
+import {PokemonsContext} from 'context/pokemonsContext';
 
 import StartPage  from './routes/Start';
 import BoardPage  from './routes/Board';
@@ -6,13 +9,35 @@ import FinishPage from './routes/Finish';
 
 
 const GamePage = () => {
+    const [selectedPokemons, setSelectedPokemons] = useState({});
+
+    const handleUpdate = (key, pokemon) =>
+        setSelectedPokemons(prevState => {
+            prevState = {...prevState};
+            prevState[key] = pokemon;
+            return prevState;
+        })
+
+    const handleRemove = (key) =>
+        setSelectedPokemons(prevState => {
+            prevState = {...prevState};
+            delete prevState[key];
+            return prevState;
+        })
+
     const match = useRouteMatch();
     return (
-        <Switch>
-            <Route path={`${match.path}/`} exact component={StartPage}/>
-            <Route path={`${match.path}/board`} component={BoardPage}/>
-            <Route path={`${match.path}/finish`} component={FinishPage} />
-        </Switch>
+        <PokemonsContext.Provider value={{
+            selected: selectedPokemons,
+            update: handleUpdate,
+            remove: handleRemove
+        }}>
+            <Switch>
+                <Route path={`${match.path}/`} exact component={StartPage}/>
+                <Route path={`${match.path}/board`} component={BoardPage}/>
+                <Route path={`${match.path}/finish`} component={FinishPage}/>
+            </Switch>
+        </PokemonsContext.Provider>
     );
 };
 
