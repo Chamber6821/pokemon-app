56e54d15ea4563248925ede12ed9d5c32c11f988
Chamber6821 <vania.osinin@yandex.ru>
2021-02-10 00:33:56 +0300

    refacoring(firebase): FIrebaseContext

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
+import Firebase          from 'services/firebase';
+import {FirebaseContext} from 'context/firebaseContext';
+
 import App             from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 import './index.css';
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <FirebaseContext.Provider value={new Firebase()}>
+            <App/>
+        </FirebaseContext.Provider>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 reportWebVitals();
