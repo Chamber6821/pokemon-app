564b996ea6cb992073753a6f5981b0a11902c065
Chamber6821 <vania.osinin@yandex.ru>
2021-02-14 22:54:26 +0300

    feature(FinishPage): task #1

--- src/routes/Game/routes/Finish/index.js
+++ src/routes/Game/routes/Finish/index.js
@@ -1,7 +1,44 @@
+import cn           from 'classnames';
+import {useContext} from 'react';
+import {useHistory} from 'react-router-dom';
+
+import {GameContext} from 'context/gameContext';
+import PokemonCard   from 'components/PokemonCard';
+import PrimaryButton from 'components/PrimaryButton';
+
+import s from './style.module.css';
+
 const FinishPage = () => {
+    const history = useHistory();
+    const gameContext = useContext(GameContext);
+
+    if (!gameContext.isGameOver) history.replace('/game');
+
     return (
-        <div>
-            <h1 style={{textAlign: 'center'}}>FinishPage</h1>
+        <div className={s.verticalContainer}>
+            <div className={cn(s.horizontalContainer, s.item)}>
+                {
+                    gameContext.myCards.map((item) => <PokemonCard
+                        key={item.id}
+                        className={cn(s.card, s.item)}
+                        data={{...item, possession: false}}
+                    />)
+                }
+            </div>
+            <div className={s.item}>
+                <PrimaryButton to="/game">
+                    End Game
+                </PrimaryButton>
+            </div>
+            <div className={cn(s.horizontalContainer, s.item)}>
+                {
+                    gameContext.opponentCards.map((item) => <PokemonCard
+                        key={item.id}
+                        className={cn(s.card, s.item)}
+                        data={{...item, possession: false}}
+                    />)
+                }
+            </div>
         </div>
     );
 };
